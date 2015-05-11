7bd4fc74abce19e068fe8b110dd9635b9ee7267b
chase & ian <student@epicodus.com>
2015-05-11 16:43:38 -0700

    add employee to division page, update employee name.

--- app.rb
+++ app.rb
@@ -10,3 +10,51 @@ get("/") do
   @divisions = Division.all()
   erb(:index)
 end
+
+get("/divisions/:id/edit") do
+  @division = Division.find(params.fetch("id").to_i())
+  erb(:division_edit)
+end
+
+post("/divisions") do
+  division = params.fetch("division")
+  new_division = Division.new(:division => division, :id => nil)
+  new_division.save()
+  erb(:division_success)
+end
+
+patch("/divisions/:id") do
+  division = params.fetch("division")
+  @division = Division.find(params.fetch("id").to_i())
+  @division.update({:division => division})
+  erb(:division_success)
+end
+
+get("/divisions/:id") do
+  @division = Division.find(params.fetch("id").to_i())
+  @employee = Employee.find(params.fetch("id").to_i())
+  erb(:division)
+end
+
+post("/employees") do
+  @divisions = Division.all()
+  name = params.fetch("name")
+  division_id = params.fetch("division_id").to_i()
+  @division = Division.find(division_id)
+  @employee = Employee.new({:name => name, :division_id => division_id})
+  @employee.save()
+  erb(:index)
+end
+
+patch("/employees/:id") do
+  @divisions = Division.all()
+   name = params.fetch("name")
+   @employees = Employee.find(params.fetch("id").to_i())
+   @employees.update({:name => name})
+  erb(:index)
+end
+
+get("/employees/:id/edit") do
+  @employee = Employee.find(params.fetch("id").to_i())
+  erb(:employee_edit)
+end
